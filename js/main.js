d6d94d678490dddba6d745ed88d7c16b09e703ee
buchajgucha <sportnashevse@yandex.ru>
2022-06-03 19:49:44 +0300

    commit final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+$(function () {
+  $('.photogallery__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrowleft.svg" alt=""/></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrowright.svg" alt=""/></button>'
+  });
 
+  $('.about__popup').on('click', function (event) {
+    event.stopPropagation()
+    $('.video__play').toggleClass('video__play--active');
+    $('.about__img').toggleClass('about__img--active');
+    $('.about__popup-img').toggleClass('about__popup-img--active');
+    if ($('.video__play').hasClass('video__play--active')) {
+      $('.video__play').trigger('play');
+
+    } else {
+      $('.video__play').trigger('pause')
+    }
+
+  });
+  $('.video__play').on('click', function (event) {
+    event.preventDefault()
+  })
 });
