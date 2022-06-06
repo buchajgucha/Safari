fb49938612e9739202b731619ceecb3d63d62d14
buchajgucha <sportnashevse@yandex.ru>
2022-06-06 17:14:37 +0300

    final

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ $(function () {
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrowright.svg" alt=""/></button>'
   });
 
-  $('.about__popup').on('click', function (event) {
-    event.stopPropagation()
+  $('.about__popup').on('click', function() {
     $('.video__play').toggleClass('video__play--active');
     $('.about__img').toggleClass('about__img--active');
     $('.about__popup-img').toggleClass('about__popup-img--active');
