1135f0f9ed953a9f395710574d15b5d3e0f51d56
buchajgucha <sportnashevse@yandex.ru>
2022-08-11 19:17:55 +0300

    adaptive?

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 $(function () {
-  $('.photogallery__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrowleft.svg" alt=""/></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/arrowright.svg" alt=""/></button>'
+  $(".photogallery__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrowleft.svg" alt=""/></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrowright.svg" alt=""/></button>',
   });
 
-  $('.about__popup').on('click', function() {
-    $('.video__play').toggleClass('video__play--active');
-    $('.about__img').toggleClass('about__img--active');
-    $('.about__popup-img').toggleClass('about__popup-img--active');
-    if ($('.video__play').hasClass('video__play--active')) {
-      $('.video__play').trigger('play');
-
+  $(".about__popup").on("click", function () {
+    $(".video__play").toggleClass("video__play--active");
+    $(".about__img").toggleClass("about__img--active");
+    $(".about__popup-img").toggleClass("about__popup-img--active");
+    if ($(".video__play").hasClass("video__play--active")) {
+      $(".video__play").trigger("play");
     } else {
-      $('.video__play').trigger('pause')
+      $(".video__play").trigger("pause");
     }
-
   });
-  $('.video__play').on('click', function (event) {
-    event.preventDefault()
-  })
+  $(".video__play").on("click", function (event) {
+    event.preventDefault();
+  });
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleClass("menu__list--active");
+  });
 });
